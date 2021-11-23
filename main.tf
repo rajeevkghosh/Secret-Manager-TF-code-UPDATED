c765f8e3f317927098f42008122da9411aa54cbc
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-11-23 14:25:06 +0530

    1

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+access_token = var.access_token
+}
+resource "google_secret_manager_secret" "secret-basic" {
+  secret_id = "us-dev-abcd-fghi-secret1"
+
+    labels = {
+    env                  = "default"
+    application_division = "pci",
+    application_name     = "demo",
+    application_role     = "app",
+    au                   = "0223092",
+    created              = "20211122",
+    data_compliance      = "pci",
+    data_confidentiality = "pub",
+    data_type            = "test",
+    environment          = "dev",
+    gcp_region           = "us",
+    owner                = "hybridenv",
+  }
+
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+      replicas {
+        location = "us-east1"
+      }
+    }
+  }
+}
