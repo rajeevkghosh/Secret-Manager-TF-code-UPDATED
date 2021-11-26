575f890df465f06fa33f9562cb8728d5b1a75118
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-11-26 16:24:46 +0530

    6

--- main.tf
+++ main.tf
@@ -1,13 +1,45 @@
 provider "google" {
   access_token = var.access_token
+  project   = "airline1-sabre-wolverine"
 }
+
+resource "google_kms_key_ring" "keyring1" {
+  name     = "keyring-example-1"
+  location = "us-central1"
+}
+
+resource "google_kms_crypto_key" "nav-key1" {
+  name            = "cryopt-key-nav1"
+  key_ring        = google_kms_key_ring.keyring1.id
+  rotation_period = "7776000s"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "google_kms_key_ring" "keyring2" {
+  name     = "keyring-example-2"
+  location = "us-east1"
+}
+
+resource "google_kms_crypto_key" "nav-key2" {
+  name            = "cryopt-key-nav2"
+  key_ring        = google_kms_key_ring.keyring2.id
+  rotation_period = "7776000s"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "google_secret_manager_secret" "secret-basic" {
   secret_id = "us-dev-abcd-fghi-secret1"
-  project   = "airline1-sabre-wolverine"
+  
 
   labels = {
     env                  = "default"
- #   application_division = "pci",
+    application_division = "pci",
     application_name     = "demo",
     application_role     = "app",
     au                   = "0223092",
@@ -20,14 +52,15 @@ resource "google_secret_manager_secret" "secret-basic" {
     owner                = "hybridenv",
   }
 
-
   replication {
     user_managed {
       replicas {
         location = "us-central1"
+        kms_key_name = google_kms_crypto_key.nav-key1.id
       }
       replicas {
         location = "us-east1"
+        kms_key_name = google_kms_crypto_key.nav-key2.id
       }
     }
   }
