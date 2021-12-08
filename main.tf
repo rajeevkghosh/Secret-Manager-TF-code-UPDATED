009e57ac9d01b0a921ca73bd2e7da69e9e952b9f
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-12-08 15:30:18 +0530

    1

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "google_secret_manager_secret" "secret-basic" {
   replication {
     user_managed {
       replicas {
-        location = "us-central1"
+        location = "asia-south1"
       }
       replicas {
         location = "us-east1"
